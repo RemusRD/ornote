d0e2193c7a14f146888a380f0ecb57adb881e745
Remus Richard Dumitrache <Remus.Richard@autentia.com>
2018-08-07 14:15:31 +0200

    It seems it compiles now

--- src/main/kotlin/ornote/app/Application.kt
+++ src/main/kotlin/ornote/app/Application.kt
@@ -1,30 +1,20 @@
 package ornote.app
-
 import io.ktor.server.netty.*
 import io.ktor.routing.*
 import io.ktor.application.*
-import io.ktor.features.CORS
-import io.ktor.features.ContentNegotiation
-import io.ktor.features.DefaultHeaders
 import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.server.engine.*
-import java.time.Duration
-
-fun Application.main(){
-        install(DefaultHeaders)
-        install(CORS) {
-            maxAge = Duration.ofDays(1)
-        }
-
-        routing {
-            get("/") {
-                println("Hola")
-                call.respond("Hello, world!")
-            }
-        }
-}
 
 fun main(args: Array<String>) {
-    embeddedServer(Netty, 8080, module = Application::main).start(wait = true)
+    val server = embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/") {
+                call.respondText("Hello World!", ContentType.Text.Plain)
+            }
+            get("/demo") {
+                call.respondText("HELLO WORLD!")
+            }
+        }
+    }.start()
 }
--- src/test/kotlin/ornote/HelloTest.kt
+++
@@ -1,8 +0,0 @@
-package ornote
-
-import org.junit.Test
-import kotlin.test.assertEquals
-
-class HelloTest {
-
-}
